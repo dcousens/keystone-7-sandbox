998e0cf807a1bb6db7926bce6aefb09584a36b9b
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-10-02 16:08:54 +1100

    add third pass, now with a GraphQL schema and resolvers

--- packages/internals/index.ts
+++ packages/internals/index.ts
@@ -1,15 +1,81 @@
 import type { Configuration, Context } from './types'
+import {
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql'
+import type { PrismaClient } from '@prisma/client'
+import { printSchema } from 'graphql'
 
-export function setup (config: Configuration): Context {
+const PRISMA_GRAPHQL_TYPE_MAPPING = {
+  'Int': GraphQLInt,
+  'String': GraphQLString
+}
+
+export function setup (prisma: PrismaClient, {
+  lists
+}: Configuration): Context {
+  console.error({ lists })
+
+  const Query = new GraphQLObjectType<undefined, number>({
+    name: 'Query',
+    fields: {
+      ...[...(function* () {
+        for (const [listKey, listConfig] of Object.entries(lists)) {
+//            console.error({ listKey, listConfig })
+
+          const { fields } = listConfig
+          const listType = new GraphQLObjectType({
+            name: listKey,
+            fields: {
+              ...[...(function* () {
+                for (const [fieldKey, fieldConfig] of Object.entries(fields)) {
+//                    console.error({ fieldKey, fieldConfig })
+                  if (!fieldConfig) continue // ...? Typescript
+
+                  const mappedType = PRISMA_GRAPHQL_TYPE_MAPPING[fieldConfig.prisma.type]
+
+                  yield {
+                    [fieldKey]: {
+                      type: new GraphQLNonNull(mappedType),
+                      resolve: (parent: any) => parent[fieldKey],
+                    },
+                  }
+                }
+              }())].reduce((a, x) => ({ ...a, ...x }), {})
+            },
+          })
+
+          const mappedIdType = listConfig.fields.id.prisma.type
+          yield {
+            [listKey]: {
+              type: listType,
+              args: {
+                id: {
+                  type: new GraphQLNonNull(GraphQLString)
+                }
+              },
+              resolve: async (parent: any, args: {
+                id: string
+              }) => {
+                return await prisma.post.findUnique({
+                  where: { id: args.id },
+                })
+              },
+            },
+          }
+        }
+      }())].reduce((a, x) => ({ ...a, ...x }), {})
+    },
+  })
+
+  const schema = new GraphQLSchema({
+    query: Query,
+  })
+
+  console.log({ gql: printSchema(schema) })
   return {
-    graphql: {
-      execute: async () => {
-
-      }
-    },
-
-    prisma: {
-
-    },
   }
 }
--- packages/internals/types.ts
+++ packages/internals/types.ts
@@ -1,41 +1,36 @@
+type __FieldConfiguration = {
+  graphql: {
+  }
+  prisma: {
+    type: 'Int' | 'String'
+    modifiers: {
+      optional?: boolean
+      array?: boolean
+    },
+    attributes: {
+      id?: boolean,
+      default?: string
+    },
+  }
+  hooks: {
+    resolveInput: () => Promise<unknown>
+    beforeOperation: () => Promise<unknown>
+    afterOperation: () => Promise<unknown>
+  }
+}
 
 export type Configuration = {
   lists: {
     [key: string]: {
       fields: {
-        [key: string]: {
-          prisma: {
-            type: 'Int' | 'String'
-            modifiers: {
-              optional?: boolean
-              array?: boolean
-            },
-            attributes: {
-              id?: boolean,
-              default?: string
-            },
-          }
-          graphql: {
-          }
-          hooks: {
-            resolveInput: () => Promise<unknown>
-            beforeOperation: () => Promise<unknown>
-            afterOperation: () => Promise<unknown>
-          }
-        }
-      }
+        id: __FieldConfiguration
+      } & Partial<{
+        [key: string]: __FieldConfiguration
+      }>
     }
   }
 }
 
+export type ListConfiguration = Configuration['lists'][string]
 export type FieldConfiguration = Configuration['lists'][string]['fields'][string]
-
-export type Context = {
-  graphql: {
-    execute: () => Promise<void>
-  }
-
-  prisma: {
-
-  }
-}
+export type Context = {}
