22a4b7a595d01926e69966a899db9047eacd67a2
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-10-02 15:32:22 +1100

    fix prisma dependencies

--- examples/entry/main.ts
+++ examples/entry/main.ts
@@ -1,11 +1,9 @@
 import { setup } from '@keystone-7/internals'
 import { id, text } from '@keystone-7/internals/fields'
-
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from './.myprisma/client'
 
 async function main () {
   const prisma = new PrismaClient()
-
   const context = setup(prisma, {
     lists: {
       Post: {
